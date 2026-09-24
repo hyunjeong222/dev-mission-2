91a9522b734150f3bd96a9bb0f195273575f71f8
jeongii <hyenj629@naver.com>
2026-09-24 18:33:58 +0900

    Initial commit

---
+++ src/main/java/com/back/DevMission2Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMission2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevMission2Application.class, args);
+    }
+
+}
